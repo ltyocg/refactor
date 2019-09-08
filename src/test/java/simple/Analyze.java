8caaa365c2a1106591c3be6ffd897870a8c62218
John Liu <ltyocg@163.com>
2019-09-08 22:32:43 +0800

    README

--- src/test/java/simple/Analyze.java
+++ src/test/java/simple/Analyze.java
@@ -1,9 +1,13 @@
 package simple;
 
-import com.github.javaparser.JavaParser;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.expr.BinaryExpr;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 import org.junit.jupiter.api.Test;
 
 class Analyze {
@@ -20,7 +24,25 @@ class Analyze {
     }
 
     @Test
-    void other(){
-        JavaParser javaParser=new JavaParser();
+    void analysis() {
+        // Set up a minimal type solver that only looks at the classes used to run this sample.
+        CombinedTypeSolver combinedTypeSolver = new CombinedTypeSolver();
+        combinedTypeSolver.add(new ReflectionTypeSolver());
+
+        // Configure JavaParser to use type resolution
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(combinedTypeSolver);
+        StaticJavaParser.getConfiguration().setSymbolResolver(symbolSolver);
+
+        // Parse some code
+        CompilationUnit cu = StaticJavaParser.parse("class X { int x() { return 1 + 1.0 - 5; } }");
+
+        // Find all the calculations with two sides:
+        cu.findAll(BinaryExpr.class).forEach(be -> {
+            // Find out what type it has:
+            ResolvedType resolvedType = be.calculateResolvedType();
+
+            // Show that it's "double" in every case:
+            System.out.println(be.toString() + " is a: " + resolvedType);
+        });
     }
 }
