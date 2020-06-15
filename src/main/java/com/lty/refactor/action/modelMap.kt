6f72342dae7915f49d6e9931b0270e6ba60908e8
ltyocg <81881627@qq.com>
2020-06-15 14:51:10 +0800

    Code buffer.

--- src/main/java/com/lty/refactor/action/modelMap.kt
+++ src/main/java/com/lty/refactor/action/modelMap.kt
@@ -6,14 +6,23 @@ import com.github.javaparser.symbolsolver.JavaSymbolSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JarTypeSolver
 import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver
+import com.github.javaparser.utils.SourceRoot
 import java.io.File
 
 fun main() {
-    val jarTypeSolver = JarTypeSolver("D:\\Program Files\\apache-maven-3.6.3\\repository\\in-project\\jpower\\3.7\\jpower-3.7.jar")
-    val javaSymbolSolver = JavaSymbolSolver(CombinedTypeSolver(jarTypeSolver, JavaParserTypeSolver("E:\\workplace\\maven\\Inspur.Dzzw.CloudAccept_V3.5\\src\\main\\java")))
-    StaticJavaParser.getConfiguration().setSymbolResolver(javaSymbolSolver)
-    val compilationUnit = StaticJavaParser.parse(File("E:\\workplace\\maven\\Inspur.Dzzw.CloudAccept_V3.5\\src\\main\\java\\com\\inspur\\accept\\monitor\\allbusiness\\action\\AllBusinessController.java"))
-    compilationUnit.findAll(ClassOrInterfaceDeclaration::class.java).forEach {
-        println(it.extendedTypes[0].resolve().qualifiedName)
+    val combinedTypeSolver = CombinedTypeSolver(
+            JarTypeSolver("D:\\Program Files\\apache-maven-3.6.3\\repository\\in-project\\jpower\\3.7\\jpower-3.7.jar"),
+            JavaParserTypeSolver("E:\\workplace\\maven\\Inspur.Dzzw.CloudAccept_V3.5\\src\\main\\java"),
+            ReflectionTypeSolver()
+    )
+    val sourceRoot = SourceRoot(File("E:\\workplace\\maven\\Inspur.Dzzw.CloudAccept_V3.5\\src\\main\\java").toPath(), StaticJavaParser.getConfiguration().apply {
+        setSymbolResolver(JavaSymbolSolver(combinedTypeSolver))
+        isLexicalPreservationEnabled = true
+    })
+    sourceRoot.tryToParse("com.inspur.accept.systemsetting.notice.action", "AcceptNoticeController.java").result.get().findAll(ClassOrInterfaceDeclaration::class.java).forEach { classOrInterfaceDeclaration ->
+        val resolve = classOrInterfaceDeclaration.extendedTypes[0].resolve()
+        println(resolve.qualifiedName)
+        val typeDeclaration = resolve.typeDeclaration.get()
     }
 }
